4329f7c0b105e0253ba824f6e9a3bd0d438061d3
Tomas Fabrizio Orsi <torsi@fi.uba.ar>
2025-08-24 20:43:27 -0300

    feat: shell script that generates docker compose files
    Signed-off-by: Tomas Fabrizio Orsi <torsi@fi.uba.ar>

---
+++ generar-compose.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+archivo=$1
+cantidad=$2
+
+compose_template="name: tp0
+services:
+  server:
+    container_name: server
+    image: server:latest
+    entrypoint: python3 /main.py
+    environment:
+      - PYTHONUNBUFFERED=1
+      - LOGGING_LEVEL=DEBUG
+    networks:
+      - testing_net
+"
+
+# $1: Numero del cliente
+generate_client() {
+    client_name="client"
+    client_name+="${1}"
+    client_template="
+  ${client_name}:
+    container_name: ${client_name}
+    image: client:latest
+    entrypoint: /client
+    environment:
+      - CLI_ID=${1}
+      - CLI_LOG_LEVEL=DEBUG
+    networks:
+      - testing_net
+    depends_on:
+      - server
+"
+    compose_template+="${client_template}"
+}
+
+
+for i in $(seq 1 $cantidad); do
+    generate_client $i
+done
+
+compose_template+="
+networks:
+  testing_net:
+    ipam:
+      driver: default
+      config:
+        - subnet: 172.25.125.0/24
+"
+
+# We write the resulting compose to a file
+echo "${compose_template}" > $archivo
